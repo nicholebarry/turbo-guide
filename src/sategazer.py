a8a24519de19323480064e2ab5c33383af3e0174
JasonAhumada <a1746103@adelaide.edu.au>
2023-09-06 21:03:23 +0930

    proof of concept works - but position is wrong

--- src/sategazer.py
+++ src/sategazer.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-""" Detect satellites passing over the FoV of astronomical observations
+""" Detect satellites passing over the range of astronomical observations
 
 """
 
@@ -13,94 +13,159 @@ from astropy.coordinates import SkyCoord
 from astropy.table import Table
 from astropy import units as u
 
-def convert_coord_to_deg(source_coords):
-    """ Convert coordinate strings to decimal degrees format
+# requesting satellit TLE data  
+import requests
 
-    Parameters
-    ----------
-    source_coords : string
-        RA and Dec
+# convert string to valid filename 
+from slugify import slugify
 
-    Returns
-    -------
-    source_pos: 
-        _description_
+# Defining location of observer
+from astropy.coordinates import EarthLocation
+
+# For start time and end time
+from datetime import datetime, timedelta
+
+# Using sky field to load the TLE data
+from skyfield.api import load
+
+# Using skyfield to define the observer location
+from skyfield.toposlib import Topos
+
+# specifying timezone of observer
+import pytz
+
+class Observer:
+    def __init__(self, name, latitude, longitude, elevation):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.elevation = elevation
+
+        self.location = EarthLocation(lat=latitude, lon=longitude, height=elevation)
+        self.start_time = None
+        self.end_time = None
+        self.time_step = None
+        self.azimuth_range = None
+        self.elevation_range = None
+
+    def set_observer_times(self, start_time, end_time, time_step):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.time_step = time_step
+
+    def set_observer_range(self, elevation_range, azimuth_range):
+        self.elevation_range = elevation_range
+        self.azimuth_range = azimuth_range
+
+class Satellite:
+    def __init__(self, name):
+        self.name = name
+        self.TLE_data = None
+
+    def set_TLE_data(self, TLE_data):
+        self.TLE_data = TLE_data
+
+def initialise_observer(latitude, longitude, elevation):
+    """ Define the observer location wiht astropy
+
+    Args:
+        latitude (degree): _description_
+        longitude (degree): _description_
+        elevation (metres): _description_
+
+    Returns:
+        _type_: _description_
     """
-    if ':' in source_coords:
-        source_pos = SkyCoord(source_coords, unit=(u.hourangle, u.deg))
-    else:
-        source_pos = SkyCoord(source_coords, unit=(u.deg, u.deg))
+    observer_location = EarthLocation(lat=latitude, lon=longitude, height=elevation)
 
-    return source_pos
+    return observer_location
 
+def check_satellite_in_range(altitude, azimuth, azimuth_range, elevation_range):
+    """
+    Check if a position (given by altitude and azimuth) is within the specified range.
 
-def calculate_satellite_dist(centre_pos, time_start):
-    """ Calculate satellites' distances from the observing pointing centre
+    Args:
+        altitude (float): The altitude angle in degrees.
+        azimuth (float): The azimuth angle in degrees.
+        azimuth_range (tuple): A tuple containing the minimum and maximum azimuth angles in degrees.
+        elevation_range (tuple): A tuple containing the minimum and maximum elevation angles in degrees.
 
-    Parameters
-    ----------
-    centre_pos : 
-        an array of ra and dec of the pointing centre  in decimal degrees
-    time_start : string 
-        observation time (UTC) e.g. '2023/09/04 00:55:37.0')
+    Returns:
+        bool: True if the position is within the specified range, False otherwise.
+    """
+    min_azimuth, max_azimuth = azimuth_range
+    min_elevation, max_elevation = elevation_range
 
-    Returns
-    -------
-    sat_dists : float
-        an array of distances of satellites 
-    sat_names : string array
-        an arry of satellites names
+    is_in_azimuth_range = min_azimuth <= azimuth <= max_azimuth
+    is_in_elevation_range = min_elevation <= altitude <= max_elevation
+
+    return is_in_azimuth_range and is_in_elevation_range
+
+def calculate_satellite_position_in_range(observer_object, satellite_object):
+
+    # url for satellite object in the TLE format
+    # station_url = f"https://celestrak.org/NORAD/elements/gp.php?NAME={satellite_object.name}&FORMAT=TLE"
+    all_station_url = 'http://celestrak.org/NORAD/elements/stations.txt'
+    all_satellites = load.tle_file(all_station_url)
+
+    # create set of satellite
+    all_satellite_sorted_by_names = {sat.name: sat for sat in all_satellites}
+    TLE_data = all_satellite_sorted_by_names[satellite_object.name]
+    
+    observation_times = np.arange(observer_object.start_time, observer_object.end_time, observer_object.time_step).astype(datetime)
+
+    # Defining location as a Topos class from skyfield
+    observer_location_skyfield = Topos(latitude_degrees=observer_object.latitude, longitude_degrees=observer_object.longitude, elevation_m=observer_object.elevation)
+    
+    # list to store observed satellite positions
+    observed_satellite_positions = []
+    
+    for current_observation_time in observation_times:
+        # Creating a timescale object from skyfield library to convert datetime into skyfield time format
+        timescale = load.timescale()
+        print('current_observation_time (utc):', current_observation_time)
+        # creating skyfield appropriate current_observation_time
+        current_observation_time_skyfield = timescale.utc(current_observation_time.year, current_observation_time.month, current_observation_time.day, current_observation_time.hour, current_observation_time.minute, current_observation_time.second)
         
-    """
-    # Read TLE file to get satellite information
-    file_tle = './data/satellites.txt'
-    with open(file_tle) as f:
-        tle = f.read().strip().split('\n')
-    
-    # Convert a list of TLE to a numpy array
-    arr_indx = int(len(tle) / 3), 3
-    tle_arr = np.array(tle)
-    tle_arr = tle_arr.reshape(arr_indx)
-    
-    # Caculate distances of satellites from observing centre
-    sat_names = []
-    sat_dists = []
-    for i in range(arr_indx[0]):
-        sat = ephem.readtle(tle_arr[i][0], tle_arr[i][1], tle_arr[i][2])
-        sat.compute(time_start)
-        sat_ra = ephem.degrees(sat.ra)
-        sat_dec = ephem.degrees(sat.dec)
-        sat_pos = SkyCoord(sat_ra, sat_dec, unit=(u.deg, u.deg))
-        sat_dists.append(centre_pos.separation(sat_pos).value)
-        sat_names.append(tle_arr[i][0])
-    
-    return sat_dists, sat_names
+        # TLE data normally contains  
+        topocentric_position = (TLE_data - observer_location_skyfield).at(current_observation_time_skyfield)
 
+        # Checking if satellite is in the field of view of the observer
 
-def sategazer_parser():
-    """ Configure inputs for sategazer
+        altitude, azimuth, distance = topocentric_position.altaz()
+        print(azimuth, altitude)
+        if check_satellite_in_range(altitude.degrees, azimuth.degrees, observer_object.azimuth_range, observer_object.elevation_range):
+            observed_satellite_positions.append((current_observation_time_skyfield, altitude.degrees, azimuth.degrees))
 
-    Returns
-    -------
-    parser : argparse.ArgumentParser
-        The parser for sategazer
-    """
-    parser = ArgumentParser(description='Run satgazer to search for satellites passing over the FoV')
-    parser.add_argument('-c','--coordinates', dest='coords', required='true', help='RA and Dec of observing centre separated with a space', type=str)
-    parser.add_argument('-f','--fov', dest='fov', required ='true', help='FoV of an observatios in degree', type=float)
-    parser.add_argument('-t','--obstime', dest='obstime', required='true', help='Start time of observation in UTC e.g. 2023/09/04 00:55:37.0', type=str)
-    parser.add_argument('-i','--integration', dest='obsduration', required='true', help='Integration time in hr')
-    
-    return parser
+    return observed_satellite_positions
 
 def main():
-    # Get inputs
-    parser = sategazer_parser()
-    params = parser.parse_args()
+    
+    # Creating curtin university observer object 
+    ## Curtin university is UTC+8 (8 hours ahead of UTC)
+    curtin_timezone = pytz.timezone('Etc/GMT-8')
+    curtin_university_observer_object = Observer(name='Curtin', latitude=-32.0061951, longitude=115.8944182, elevation=17.92)
 
-    cenpos = convert_coord_to_deg(params.coords)
-    sat_dis, sat_nam = calculate_satellite_dist(cenpos, params.obstime)
-    print(cenpos)
+    # Using the local time with this 
+    observation_start_time = datetime(2023, 9, 6, 19, 26, 0, tzinfo=curtin_timezone)
+    observation_end_time = datetime(2023, 9, 6, 19, 36, 0, tzinfo=curtin_timezone)
+    observation_time_step = timedelta(minutes=5)
+
+    curtin_university_observer_object.set_observer_times(observation_start_time, observation_end_time, observation_time_step)
+    curtin_university_observer_object.set_observer_range(elevation_range=[0,60], azimuth_range=[0,60])
+
+    # Defining satellite object
+    ISS_satellite_object = Satellite("ISS (ZARYA)")
+
+    satellite_objects = [ISS_satellite_object]
+
+    for satellite in satellite_objects:
+
+        satellite_position_in_curtin_university_range = calculate_satellite_position_in_range(curtin_university_observer_object, satellite)
+
+        print(satellite_position_in_curtin_university_range)
+
+    return 
 
 if __name__ == "__main__":
     main()
